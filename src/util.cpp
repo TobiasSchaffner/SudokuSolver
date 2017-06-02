1799d1fa4ba03670802d2107433024ecd7c57127
Johannes <johannesbruehl@googlemail.com>
2017-06-02 13:36:23 +0200

    Fehler in Print behoben

--- src/util.cpp
+++ src/util.cpp
@@ -2,8 +2,9 @@
 #include "../include/util.h"
 
 void print2DArray(int **array, int size) {
-    for (unsigned int column = 0; column < size; ++column) {
-        for (unsigned int row = 0; row < size; ++row) {
+    for (unsigned int row = 0; row < size; ++row) {
+        for (unsigned int column = 0; column < size; ++column) {
+
             printf("%2d ", array[column][row]);
         }
         printf("\n");
