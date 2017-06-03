23a76c6c01f4b8f399391862a62a81737ce2d447
Johannes <johannesbruehl@googlemail.com>
2017-06-03 09:57:38 +0200

    Tests für Gameboard ausgebaut, 2 offene Fragen in Todos gepackt.

--- test/src/gameboard-test.cpp
+++ test/src/gameboard-test.cpp
@@ -16,10 +16,29 @@ protected:
     }
 
     static Gameboard* gb;
+
 };
 
 Gameboard* GameBoardTest::gb = NULL;
+static constexpr int nearlyfullGameboard[9][9] = {{ 7,  0,  4,  2,  5,  3,  8,  9,  6},
+                                                  { 2,  0,  0,  6,  7,  9,  4,  1,  5},
+                                                  { 6,  0,  0,  1,  4,  8,  3,  7,  2},
+                                                  { 1,  4,  2,  8,  6,  5,  7,  3,  9},
+                                                  { 5,  7,  3,  4,  9,  2,  1,  6,  8},
+                                                  { 9,  8,  6,  3,  1,  7,  2,  5,  4},
+                                                  { 4,  2,  1,  9,  3,  6,  5,  8,  7},
+                                                  { 8,  6,  5,  7,  2,  1,  9,  4,  3},
+                                                  { 3,  9,  7,  5,  8,  4,  6,  2,  0}};
 
+static constexpr int fullGameboard[9][9] = {{ 7,  1,  4,  2,  5,  3,  8,  9,  6},
+                                            { 2,  3,  8,  6,  7,  9,  4,  1,  5},
+                                            { 6,  5,  9,  1,  4,  8,  3,  7,  2},
+                                            { 1,  4,  2,  8,  6,  5,  7,  3,  9},
+                                            { 5,  7,  3,  4,  9,  2,  1,  6,  8},
+                                            { 9,  8,  6,  3,  1,  7,  2,  5,  4},
+                                            { 4,  2,  1,  9,  3,  6,  5,  8,  7},
+                                            { 8,  6,  5,  7,  2,  1,  9,  4,  3},
+                                            { 3,  9,  7,  5,  8,  4,  6,  2,  1}};
 
 TEST(Gameboard, GameBoardInitalTest) {
     EXPECT_NE(new Gameboard(9), nullptr);
@@ -98,7 +117,7 @@ TEST_F(GameBoardTest, UndoAfterOneMoveResultsInEmptyBoard) {
 
 TEST_F(GameBoardTest, GetEmptyFieldReturnsTheFirstInRowWhenNoneIsSet) {
     auto move = new Move(9,9,9);
-    EXPECT_TRUE(gb->getEmptyField(move));
+    EXPECT_TRUE(gb->getFirstEmptyMove(move));
     EXPECT_TRUE(move->row == 0);
     EXPECT_TRUE(move->column == 0);
     EXPECT_TRUE(move->value == 9);
@@ -107,14 +126,63 @@ TEST_F(GameBoardTest, GetEmptyFieldReturnsTheFirstInRowWhenNoneIsSet) {
 TEST_F(GameBoardTest, GetEmptyFieldReturnsTheFirstInRowWhenOneAlreadySet) {
     gb->applyMove(new Move(0,0,1));
     auto move = new Move(9,9,9);
-    EXPECT_TRUE(gb->getEmptyField(move));
+    EXPECT_TRUE(gb->getFirstEmptyMove(move));
     EXPECT_TRUE(move->row == 0);
     EXPECT_TRUE(move->column == 1);
     EXPECT_TRUE(move->value == 9);
 }
 
 TEST_F(GameBoardTest, GetEmptyFieldReturnsFalseWhenBoardIsFull) {
-    auto generator = new Generator();
-    auto gameboard = generator->generate(9, 81);
-    EXPECT_FALSE(gameboard->getEmptyField(new Move(9,9,9)));
+    auto localGameboard = new Gameboard(9);
+    for(int row = 0; row < 9; ++row) {
+        for(int col = 0; col < 9; ++col) {
+            localGameboard->applyMove(new Move(col, row, fullGameboard[row][col]));
+        }
+    }
+
+    EXPECT_FALSE(localGameboard->getFirstEmptyMove(new Move(9,9,9)));
+}
+
+TEST_F(GameBoardTest, GetPromisingMoveReturnsExpectedField) {
+    auto localGameboard = new Gameboard(9);
+    for(int row = 0; row < 9; ++row) {
+        for(int col = 0; col < 9; ++col) {
+            localGameboard->applyMove(new Move(col, row, nearlyfullGameboard[row][col]));
+        }
+    }
+
+    auto move = new Move(1,1,1);
+    localGameboard->getPromisingMove(move);
+    printf("row: %d, column: %d", move->row, move->column);
+    EXPECT_TRUE(move->row == 8 && move->column == 8);
+}
+
+TEST_F(GameBoardTest, IsPossibleReturnsTrueWhenPossible) {
+    gb->applyMove(new Move(0,0,1));
+    EXPECT_TRUE(gb->isPossible(new Move(1,0,2)));
+}
+
+TEST_F(GameBoardTest, IsPossibleReturnsFalseWhenAlreadySet) {
+    gb->applyMove(new Move(0,0,1));
+    EXPECT_FALSE(gb->isPossible(new Move(0,0,2)));
+}
+
+TEST_F(GameBoardTest, IsPossibleReturnsFalseWhenRowHasValue) {
+    gb->applyMove(new Move(0,0,1));
+    EXPECT_FALSE(gb->isPossible(new Move(8,0,1)));
+}
+
+TEST_F(GameBoardTest, IsPossibleReturnsFalseWhenColumnHasValue) {
+    gb->applyMove(new Move(0,0,1));
+    EXPECT_FALSE(gb->isPossible(new Move(0,9,1)));
+}
+
+TEST_F(GameBoardTest, IsPossibleReturnsFalseWhenSegmentHasValue) {
+    gb->applyMove(new Move(0,0,1));
+    EXPECT_FALSE(gb->isPossible(new Move(2,2,1)));
+}
+
+TEST_F(GameBoardTest, Get2DArrayReturnsValueAtRightPosition) {
+    gb->applyMove(new Move(1,9,1));
+    EXPECT_TRUE(gb->get2DArray()[1][9] == 1);
 }
